213ee250bcb1361e2628ae266ca476f4015385ba
janse <jansimoboecker40@gmail.com>
2026-01-25 13:15:57 +0100

    jan_kartoffelpfanne.js updated

--- jan_kartoffelpfanne.js
+++ jan_kartoffelpfanne.js
@@ -1,13 +1,33 @@
-let mehl = 1 ;
-let eier = 2;
+// let mehl = 1 ;
+// let eier = 2;
+
+// function calculateAmount() {
+//     let appleContainer = document.getElementById("result-apple");
+//     let bananaContainer = document.getElementById("result-banana");
+//     let amount = document.getElementById("input-amount").value;
+
+
+//     appleContainer.innerHTML = mehl * amount;
+//     bananaContainer.innerHTML = eier * amount;
+
+// }
+
+const ingredients = [
+    { id: "potato", base: 400 },
+    { id: "feta", base: 100 },
+    { id: "onien", base: 0.5 },
+    { id: "red-paprika", base: 1 },
+    { id: "oil", base: 1 },
+    { id: "oil", base: 1 },
+];
 
 function calculateAmount() {
-    let appleContainer = document.getElementById("result-apple");
-    let bananaContainer = document.getElementById("result-banana");
-    let amount = document.getElementById("input-amount").value;
-
-
-    appleContainer.innerHTML = mehl * amount;
-    bananaContainer.innerHTML = eier * amount;
-
+    let portionSize = Number(document.getElementById("input-amount").value);
+    if (portionSize >= 1 && portionSize <= 20) {
+        ingredients.forEach(s => {
+            let value = s.base * portionSize;
+            if (s.comma) value = value.toString().replace(".", ",");
+            document.getElementById(s.id).textContent = value;
+        })
+    }
 }
