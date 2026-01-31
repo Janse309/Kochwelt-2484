17c47a81b05bffcc9d68141769966b54e38b7806
Jörg Marchner <joerg@marchner.de>
2026-01-31 20:30:36 +0100

    Korrekturen DA

--- spagetthi_script.js
+++ spagetthi_script.js
@@ -1,6 +1,18 @@
 
 function calculatePortions() {
     const portions = parseFloat(document.getElementById("portion-input").value);
+
+    if (portions < 1 || isNaN(portions)) {
+        alert("Bitte geben Sie eine Zahl zwischen 1 und 20 ein!");
+        portions = 1;
+        document.getElementById("portion-input").value = 1;
+    } 
+    // Obergrenze prüfen
+    else if (portions > 20) {
+        alert("Bitte geben Sie eine Zahl zwischen 1 und 20 ein!");
+        portions = 20;
+        document.getElementById("portion-input").value = 20;
+    }
     const basePortions = 4; // Basis-Portionen
     const tableCells = document.querySelectorAll("#ingredients-table td");
 
@@ -10,7 +22,7 @@ function calculatePortions() {
 
         // Einheit aus dem Text behalten
         const text = cell.textContent;
-        const unit = text.replace(/[0-9.,]/g, '').trim(); 
+        const unit = text.replace(/[0-9.,]/g, '').trim();
         cell.textContent = `${newAmount} ${unit}`;
     });
 }
